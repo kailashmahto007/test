d423b063a73b93761120962be8324578e4035cd2
kailashmahto007 <39325645+kailashmahto007@users.noreply.github.com>
2022-01-11 13:06:16 +0530

    cardview with buy button and image added

--- app/src/main/java/com/example/googlelogin/Profile.java
+++ app/src/main/java/com/example/googlelogin/Profile.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,18 +18,22 @@ import com.google.firebase.auth.FirebaseUser;
 public class Profile extends AppCompatActivity {
 private Button logout;
 private TextView name,email;
+private ImageView profileimage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+profileimage=findViewById(R.id.profileimage);
 logout=findViewById(R.id.logout);
 name=findViewById(R.id.name);
 email=findViewById(R.id.mail);
 
         GoogleSignInAccount signInAccount= GoogleSignIn.getLastSignedInAccount(this);
         if(signInAccount!=null)
-        {   name.setText(signInAccount.getDisplayName());
+        {   Glide.with(this)
+                .load(signInAccount.getPhotoUrl())
+                .into(profileimage);
+            name.setText(signInAccount.getDisplayName());
             email.setText(signInAccount.getEmail());
 
         }
